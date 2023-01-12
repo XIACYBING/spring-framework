daa8934c6a416d27b5f82bcd50950468f44cdf5f
wangyb <359474377@qq.com>
2023-01-12 15:40:54 +0800

    添加测试模块

---
+++ test-module/src/main/java/spring/test/AnnotationApplication.java
@@ -0,0 +1,27 @@
+package spring.test;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author wang.yubin
+ * @since 2023/1/12
+ */
+@ComponentScan
+public class AnnotationApplication {
+
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext context =
+				new AnnotationConfigApplicationContext(AnnotationApplication.class);
+
+		AnnotationApplication current = context.getBean(AnnotationApplication.class);
+
+		System.out.println("获取到当前启动类：" + current);
+		// System.out.println("：，。、、’；【】《》？：”|{}");
+	}
+
+	@Override
+	public String toString() {
+		return getClass().getName();
+	}
+}
